93be88076656831df5a0f47eb4d9deb588a1318b
Indurthisaiganesh <indurthisaiganesh@gmail.com>
2026-02-02 11:32:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-
+from SmartApi import SmartConnect
 import os
-
-print(os.getenv('API_KEY'))
-print( os.getenv('TOTP_SECRET'))
-print(os.getenv('CLIENT_CODE'))
-print(os.getenv('PASSWORD'))
+import pyotp
+API_KEY = os.getenv('API_KEY')
+TOTP_SECRET = os.getenv('TOTP_SECRET')
+CLIENT_CODE = os.getenv('CLIENT_CODE')
+PASSWORD = os.getenv('PASSWORD')
+obj = SmartConnect(api_key=API_KEY)
+totp = pyotp.TOTP(TOTP_SECRET).now()
+ses=obj.generateSession(CLIENT_CODE, PASSWORD, totp)
+print(ses)
